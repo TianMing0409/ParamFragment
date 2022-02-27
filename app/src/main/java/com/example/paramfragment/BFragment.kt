668117c742b91aece590dd670dcc546556555c75
TianMing0409 <tianmingkoh@gmail.com>
2022-02-27 20:34:05 +0800

    Initial commit

---
+++ app/src/main/java/com/example/paramfragment/BFragment.kt
@@ -0,0 +1,32 @@
+package com.example.paramfragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.navArgs
+import com.example.paramfragment.databinding.FragmentBBinding
+
+
+class BFragment : Fragment() {
+
+    private lateinit var binding:FragmentBBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_b,container,false)
+
+        val args: BFragmentArgs by navArgs()
+        val name = args.studentName
+
+        binding.tvResult.text = name
+
+        return binding.root
+    }
+
+}
